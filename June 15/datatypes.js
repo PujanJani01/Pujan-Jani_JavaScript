360c0416c996148c416427942a670ab483cc6947
Pujan <pujan.jani@saeculumsolutions.com>
2023-06-16 11:02:15 +0530

    Datatypes

--- June 15/datatypes.js
+++ June 15/datatypes.js
@@ -1,22 +1,36 @@
 
+        //  Number
+        // let x = 5;
+        // let x = 45.76;
+        // let x = 45476478748458;
 
-        // var name = "Pujan";
-        // console.log(name);
+        // String
+        // let x = "Pujan";
+        // let x = 'jani';
 
-        // var name = String("Pujan");
-        // console.log(name);
+        //Boolean
+        // let x = true;
+        // let x = false;
 
-        // var name = String(30);
-        // console.log(name);
+        // Null but in typeof output it will show object
+        // let x = null;
 
-        // var num = 30;
-        // console.log(num);
+        // Undefined
+        // let x;
 
-        // var num = BigInt(305645654252);
-        // console.log(num);
+        // Array but in typeof output it will show object
+        // let x = [10,20,30,40];
+        // let x = ["P","V","F","J"];
+        // let x = [30,"Jan",2005];
 
-        // var num =5;
-        // num="pujan";
-        // console.log(num);
+        // Object
+        // let x = {
+        //         fname: "Pujan",
+        //         lname: "Jani",
+        //         age: 18
+        // }
+       
+        console.log(x);
+        console.log(typeof x);
 
  
