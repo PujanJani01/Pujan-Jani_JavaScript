c9216e0591760c335c6ce3e49f21073dab9af246
Pujan <pujan.jani@saeculumsolutions.com>
2023-06-16 19:01:34 +0530

    June 16

---
+++ June 16/array.js
@@ -0,0 +1,14 @@
+const { split } = require("lodash");
+
+var odd = [1,3,5,7,9,11,13,15,17,19,21,23];
+
+// console.log(odd)
+// console.log(odd[4]);
+// console.log(odd[odd.length-1]);
+// console.log(odd.length);
+// console.log(odd.slice('3'));
+
+//---------------------------------------------------------------------------------------------------
+
+// var array = [ 1 ,2 , ["a","b","c"]];
+// console.log(array[2][2]);
---
+++ June 16/object.js
@@ -0,0 +1,216 @@
+// var obj1={age:12};
+// {
+//     obj2=obj1;
+//     console.log(obj2);
+// }
+
+
+// var obj1={age:12};
+// {
+//     obj2=obj1;
+//     console.log(obj1);
+// }
+
+
+// var obj1={age:12};
+// {
+//     obj2=obj1;
+//     console.log(obj2.age);
+// }
+
+
+// var obj1={age:12};
+// {
+//     obj2=obj1;
+//     console.log(obj1.age);
+// }
+
+
+// var obj1={age:24};
+// {
+//     console.log(obj1.age);
+//     obj1.age=20;
+// }
+// console.log(obj1.age);
+
+
+// var obj1={age:24};
+// {
+//     obj1.age=20;
+//     console.log(obj1.age);
+// }
+// console.log(obj1.age);
+
+
+// var obj1={
+//      fname:"Pujan",
+//      lname:"Jani"
+// }
+// obj2=obj1;
+// console.log(obj2.fname);
+
+
+// var obj1={
+//     fname:"Pujan",
+//     lname:"Jani"
+// }
+// obj2=obj1;
+// console.log(obj1.fname);
+
+
+// var obj1={
+//     fname:"Pujan",
+//     age:18
+// }
+// obj2=obj1;
+// obj2.age=20;
+// console.log(obj2.age);
+
+
+// var obj1={
+//     fname:"Pujan",
+//     age:18
+// }
+// obj2=obj1;
+// obj2.age=20;
+// console.log(obj1.age);
+
+
+// let obj1={
+//     fname:"Pujan",
+//     age:18
+// }
+// obj2=obj1;
+// obj2.age=20;
+// console.log(obj2.age);
+
+
+// const obj1={
+//     fname:"Pujan",
+//     age:18
+// }
+// obj2=obj1;
+// obj2.age=20;
+// console.log(obj2['age']);
+
+
+// var obj1={
+//     "first name": "pujan"
+// }
+// console.log(obj1["first name"]);
+
+
+// var obj={age:18};
+// obj.fname="pujan";
+// console.log(obj.fname);
+// obj.lname="Jani";
+// console.log(obj);
+
+
+// var engineer={
+//     education:[
+//         {
+//             degree:"B.tech",
+//             college:"Nirma",
+//             year: 2015,
+//             cgpa: 8.86
+//         },
+//         {
+//             degree:"M.tech",
+//             college:"PDEU",
+//             year: 2017,
+//             cgpa: 6.56
+//         }
+
+//     ]
+// }
+// console.log(engineer.education[0].college);
+// console.log(engineer.education[0].year);
+// console.log(engineer.education[1].degree);
+// console.log(engineer.education[1].cgpa);
+
+
+// var vishal={
+//     fname: "Vishal",
+//     lname: "Shukla",
+//     age: 20,
+//     parents: [
+//          {
+//             relation: "father",
+//             fname: "Praful",
+//             age: 56
+//         },
+//         {
+//             relation: "mother",
+//             fname: "Kamala",
+//             age: 40
+//         }
+//     ]
+// }
+// console.log(vishal.fname);
+// console.log(vishal.parents);
+// console.log(vishal.parents[1].fname);
+
+
+// var sou = {
+//     college: [
+//         {
+//             socet:[
+//                 {
+//                     degree:"B.tech",
+//                     branch:[
+//                         {
+//                             branchname:"CSE",
+//                             duration:"4y",
+//                             fees: 78000
+//                         },
+//                         {
+//                             branchname:"ME",
+//                             duration:"4y",
+//                             fees: 76000
+//                         },
+//                         {
+//                             branchname:"EE",
+//                             duration:"4y",
+//                             fees: 74000
+//                         }
+//                     ]
+//                 },
+//                 {
+//                     degree:"BE",
+//                     branch:[
+//                         {
+//                             branchname:"CSE",
+//                             duration:"4y",
+//                             fees: 60000
+//                         },
+//                         {
+//                             branchname:'ME',
+//                             duration:"4y",
+//                             fees: 52000
+//                         },
+//                         {
+//                             branchname:"EE",
+//                             duration:"4y",
+//                             fees: 49000
+//                         }
+//                     ]
+//                 }
+//             ]
+//         },
+//         {
+//             socca:[
+//                 {
+//                     degree:"BCA",
+//                     duration: "3y",
+//                     fees: 40000
+//                 }
+
+//             ]
+//         }
+//     ]
+// }
+// console.log(sou.college[0].socet[0].degree);
+// console.log(sou.college[0].socet[1].branch[2].fees);
+
+
