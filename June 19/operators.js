2e90228fee92d2635d0f8b7308c15b0e56c38dbc
Pujan <pujan.jani@saeculumsolutions.com>
2023-06-19 18:33:45 +0530

    June 19 - Operators

---
+++ June 19/operators.js
@@ -0,0 +1,95 @@
+
+
+// var a=3, b=10;
+
+// Arithmetic Operators
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(b/a);
+
+// console.log(a%b);
+// console.log(b%a);
+
+// console.log(a**b);
+// console.log(b**a);
+
+// console.log(a++);
+// console.log(a--);
+// console.log(++a);
+// console.log(--a);
+
+//--------------------------------------------------------------------------------------------------------------
+
+// var a = 12, b = 23;
+
+// Comparison Operators
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a===b);
+// console.log(a!==b);
+// console.log(a<b);
+// console.log(a<=b);
+// console.log(a>b);
+// console.log(a>=b);
+ 
+//--------------------------------------------------------------------------------------------------------------
+
+// Logical Operators
+
+// var a=10, b=34, c=10;
+
+// console.log(a==b && a==c);
+// console.log(a==b || a==c);
+
+// console.log(a==c);
+// console.log(!(a==c));
+
+// console.log(!'hello'); //false
+// console.log(!'');  //true
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Bitwise Operators
+
+// var a = 10 , b = 8 , c = 10;
+
+// console.log(a==b & a==c);
+// console.log(a==b | a==c);
+// console.log(a==b ^ a==c);
+// console.log(~a);
+// console.log(~b);
+// console.log(a<<b);
+// console.log(a>>b);
+// console.log(10>>>2);
+
+//--------------------------------------------------------------------------------------------------------------
+
+// Assignment Operators
+
+// var a = 12, b = 5 , c=10;
+
+// var c = a+b;
+// console.log(c);
+
+// c += a;
+// c -= a;  
+// c *= a;
+// c /= a;    
+// c %= a;  
+// console.log(c);
+
+//--------------------------------------------------------------------------------------------------------------
+// var X ;
+
+// X = 5 + 5 * 10;
+// X = (5 + 5) * 10;
+// X = 5 + 4 / 3;
+// X = 5 * 4 / 2;
+// X = 3 + 7 - 2 * 6 + 8 / 2;
+// X = 3 + ((7 - 2) * 6) + 8 / 2;
+
+// console.log(X);
+
+
