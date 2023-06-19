39ccaf4ac82abe37b82cc81b1c33d4288ad0e5e0
Pujan <pujan.jani@saeculumsolutions.com>
2023-06-19 19:05:15 +0530

    update object.js

--- June 16/object.js
+++ June 16/object.js
@@ -423,23 +423,6 @@
 //-------------------------------------------------------------------------------------------------
 
 // var obj = {
-//     p: 18,
-//     j: 19,
-//     b: 17,
-//     v: 18
-// };
-
-// let array = ['p','j','b','v'];
-// console.log(obj[array[1]]);
-// console.log(obj[array]); // undefined
-
-// var fname = 'j';
-// var fname = j;   // ERROR: j is not defined
-// console.log(obj[fname]);
-
-//-------------------------------------------------------------------------------------------------
-
-// var obj = {
 //     'first@name' : 'pujan',
 //     'last-name' : 'jani'
 
