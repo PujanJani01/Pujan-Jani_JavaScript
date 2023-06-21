dacdaa46115b7194477ff57aa09cbf7082e43de2
Pujan <pujan.jani@saeculumsolutions.com>
2023-06-21 11:01:17 +0530

    June 20  updated

---
+++ June 20/nullish_coalescing_operator.js
@@ -0,0 +1,14 @@
+// if first operator is null or undefined, it will print second operand else first operand
+// console.log("" ?? "world" );
+
+// console.log("world" ?? "" );
+
+// console.log(null ?? undefined);
+
+// console.log(undefined ?? null);
+
+// console.log(NaN ?? Number);
+
+// console.log(0 ?? 1);  // 0
+// console.log(0 || 1);  // 1
+
---
+++ June 20/predefined_function.js
@@ -0,0 +1,13 @@
+
+// console.log(isFinite(15));  //true
+
+// console.log(isNaN(15));  // false
+// console.log(isNaN("hey"));  // true
+
+// console.log( parseInt('15.254')); //15
+// console.log( parseInt(15.254));  // 15
+// console.log(parseInt('10 kg')); // 10
+
+// console.log(parseFloat('5 min per hour')); // 5
+// console.log('15.254'); // 15.254
+// console.log(15.254); // 15.254
