0bf927e91fc516a9d1589685126ca09e6e68c414
Pujan <pujan.jani@saeculumsolutions.com>
2023-06-23 19:34:16 +0530

    String Questions

---
+++ June 23/string_questions.js
@@ -0,0 +1,380 @@
+// let a = "javascript";
+// let b = a.length;
+// console.log(b);
+
+//-------------------------------------------------------------------------------------------------------
+
+// let a = 'hello';
+// let b = a.toUpperCase();
+// console.log(b);
+
+//-------------------------------------------------------------------------------------------------------
+
+// let a = "hello";
+// let b = a.indexOf('l');
+// console.log(b);
+
+//-------------------------------------------------------------------------------------------------------
+
+// let a = "java";
+// let b = a.concat('script');
+// console.log(b);
+
+//-------------------------------------------------------------------------------------------------------
+
+// let a = "hi I am pujan";
+// let b = a.slice(8);
+// let c = a.slice(8,12);
+// console.log(b);
+// console.log(c);
+
+// let a = "hi I am pujan";
+// let b = a.substring(8);
+// let c = a.substring(8,12);
+// console.log(b);
+// console.log(c);
+
+//-------------------------------------------------------------------------------------------------------
+
+// let a = "my name is khan";
+// console.log(a);
+// let b = a.replace('khan','shukla');
+// console.log(b);
+
+//-------------------------------------------------------------------------------------------------------
+
+// let a = "cow is a animal"
+// let b = a.split('s');
+// console.log(b);
+
+//-------------------------------------------------------------------------------------------------------
+
+// let a = "   hello    ";
+// console.log(a);
+// let b = a.trim();
+// console.log(b);
+
+//-------------------------------------------------------------------------------------------------------
+
+// let a = 'javascript is different than java';
+// let regex = /java/g;
+// let b = a.search(regex);
+// console.log(b);
+
+//-------------------------------------------------------------------------------------------------------
+
+// let a = "javascript is different than java";
+// let b = a.startsWith('java');
+// console.log(b);
+// let c =a.startsWith('script');
+// console.log(c);
+
+//-------------------------------------------------------------------------------------------------------
+
+// const message = "Hello, World!";
+// console.log(message.length);
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str = "HelloWorld";
+// console.log(str.toLowerCase());
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "I love JavaScript!";
+// console.log(sentence.indexOf("JavaScript"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const name = "John";
+// const greeting = `Hello, ${name}!`;
+// console.log(greeting);
+
+//-------------------------------------------------------------------------------------------------------
+
+// const text = "Hello, World!";
+// console.log(text.substring(7));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const message = "Hello, World!";
+// console.log(message.replace("World", "Universe"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const words = "apple, banana, orange";
+// const fruits = words.split(", ");
+// console.log(fruits);
+
+//-------------------------------------------------------------------------------------------------------
+
+// const text = "   Hello, World!   ";
+// console.log(text.trim());
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "The quick brown fox jumps over the lazy dog.";
+// console.log(sentence.includes("dog"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str = "Hello, World!";
+// console.log(str.endsWith("!"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is fun!";
+// console.log(sentence.charAt(4));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str1 = "Hello";
+// const str2 = "World";
+// console.log(str1.concat(", ", str2));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is awesome!";
+// console.log(sentence.slice(0, 10));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const text = "Hello, World!";
+// console.log(text.repeat(3));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "The quick brown fox";
+// console.log(sentence.split(" "))
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str = "Hello, World!";
+// console.log(str.indexOf("o"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is fun!";
+// console.log(sentence.substring(4, 10));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const text = "Hello, World!";
+//  console.log(text.charAt(7));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "I love JavaScript!";
+//  console.log(sentence.split("o"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str = "Hello, World!";
+//  console.log(str.includes("World"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is cool!";
+//  console.log(sentence.lastIndexOf("o"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const text = "Hello, World!";
+// console.log(text.startsWith("Hello"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str = "Hello, World!";
+//  console.log(str.toUpperCase());
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is awesome!";
+//  console.log(sentence.toLowerCase());
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str = "Hello, World!";
+//  console.log(str.charAt(13));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is fun!";
+// console.log(sentence.substr(4, 10));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const text = "Hello, World!";
+// console.log(text.includes("WORLD"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is amazing!";
+//  console.log(sentence.replace("amazing", "awesome"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str = "Hello, World!";
+//  console.log(str.charCodeAt(4))
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is powerful!";
+// console.log(sentence.split(" ").length);
+
+//-------------------------------------------------------------------------------------------------------
+
+// const text = "Hello, World!";
+// console.log(text.search("W"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is dynamic!";
+//  console.log(sentence.slice(-7));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str = "Hello, World!";
+//  console.log(str.concat(" Goodbye!"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is flexible!";
+//  console.log(sentence.charAt(sentence.length - 1));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is interesting!";
+// console.log(sentence.substring(10));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str = "Hello, World!";
+//  console.log(str.search("World"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is versatile!";
+// console.log(sentence.split(" ").reverse().join(" "));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is powerful!";
+// console.log(sentence.substring(4, 10));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str = "Hello, World!";
+// console.log(str.charCodeAt(0));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is amazing!";
+// console.log(sentence.slice(4, 10));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const text = "Hello, World!";
+// console.log(text.indexOf("o", 5));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is fun!";
+//  console.log(sentence.startsWith("Java"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str = "Hello, World!";
+// console.log(str.endsWith("!"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is amazing!";
+//  console.log(sentence.lastIndexOf("a", 12));
+
+//-------------------------------------------------------------------------------------------------------
+
+//  const text = "Hello, World!";
+//  console.log(text.substring(7, 2));
+
+//-------------------------------------------------------------------------------------------------------
+
+//  const sentence = "JavaScript is cool!";
+//  console.log(sentence.concat(" I love it!"));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str = "Hello, World!";
+// console.log(str.toUpperCase().charAt(7));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const str = "Hello, World!";
+// console.log(str.toLowerCase().charAt(7));
+
+//-------------------------------------------------------------------------------------------------------
+
+// const sentence = "JavaScript is amazing!";
+// console.log(sentence.replace("amazing", "awesome").toUpperCase());
+
+//-------------------------------------------------------------------------------------------------------
+
+// function is_string(input){
+//     if(typeof input==="string")
+//          return true;
+//     else return false;
+// }
+// console.log(is_string('w3resource'));   // true
+// console.log(is_string([1, 2, 4, 0]));   // false
+// console.log(is_string(null));           // false
+
+//-------------------------------------------------------------------------------------------------------
+
+// function is_Blank(input){
+//     if(input=='')
+//          return true;
+//     else return false;
+// }
+// console.log(is_Blank('')); // true
+// console.log(is_Blank('abc')); // false
+// console.log(is_Blank(""));     // true
+// console.log(is_Blank(" "));     // true
+
+//-------------------------------------------------------------------------------------------------------
+
+// function string_to_array(input) {
+//     return input.split(' ');
+// }
+ // console.log(string_to_array("Robin Singh")); // ["Robin", "Singh"]
+// console.log(string_to_array("Robin Singh chaddha")); // ["Robin", "Singh"]
+    
+//--------------------------------------------------------------------------------------------------------
+
+// function truncate_string(input,num){
+//     return input.slice(0,num);
+// }
+// console.log(truncate_string("Robin Singh",4)); // "Robi"
+
+//--------------------------------------------------------------------------------------------------------
+
+// function protect_email(email){
+//     return email.replace(email.substring(5,email.indexOf('@')),'...');
+// }
+// console.log(protect_email("robin_sing@example.com"));   
+// console.log(protect_email("pratham.pandya@example.com")); 
+
+//--------------------------------------------------------------------------------------------------------
+
+// function string_parameterize(input){
+//     return input.toLowerCase().replaceAll(" ","-");
+// }
+// console.log(string_parameterize("Robin Singh from USA."));
+// console.log(string_parameterize("Robin Singh  from USA."));
+ 
+//--------------------------------------------------------------------------------------------------------
+
+// function capitalize(input){
+//     return input.charAt(0).toUpperCase()+input.slice(1);
+// }
+// console.log(capitalize('js string exercises'));
+// console.log(capitalize('hello it is me'));
