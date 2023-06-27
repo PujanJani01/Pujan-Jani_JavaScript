d7362021fdebef97b0a88c34b5b7b6920f9c1d8e
Pujan <pujan.jani@saeculumsolutions.com>
2023-06-27 10:51:46 +0530

    Update practice.js

--- June 26/practice.js
+++ June 26/practice.js
@@ -1,4 +1,10 @@
-// let obj1 = {a:10, b:22};
+// let abc = 'hello world';
+// abc.replace('world',' ');
+// console.log(abc);
+
+
+
+ // let obj1 = {a:10, b:22};
 // let obj2 = Object.assign({a: 20}, obj1);
 // // console.log(obj2);
 // obj2.a = 30;
@@ -54,3 +60,53 @@
 // console.log(source);
 
 
+// let a = { e: 2, f:4};
+// let b = [1,2,3,4];
+// let c = Object.assign(b,a);
+// console.log(c);             // return array
+
+// let a = { e: 2, f:4};
+// let b = [1,2,3,4];
+// let c = Object.assign(a,b);
+// console.log(c);              // return object
+
+//  let obj1=[1,2,3,4];
+//  let obj2={x : { y : 3 , z:{a : 1 } } };
+//  let obj3=[5 , p = { q : 7 , r : { s : 10 } },6 , 7 , 8];
+//  let obj4=Object.assign(obj3,obj1,obj2);
+//  console.log(obj1);
+//  console.log(obj2);
+//  console.log(obj3);
+//  console.log(obj4);
+
+
+// let a ='hello';
+// let b ={d: 'world'};
+// let c = Object.assign(b,a);
+// console.log(c);         // return object
+
+// let a ='hello';
+// let b ={d: 'world'};
+// let c = Object.assign(a,b);
+// console.log(c);         // [String: 'hello'] { d: 'world' }
+
+// let a = 12012;
+// let b ={d: 'world'};
+// let c = Object.assign(a,b);
+// console.log(c);          // [Number: 12012] { d: 'world' }
+
+// let a = 12012;
+// let b ={d: 'world'};
+// let c = Object.assign(b,a);
+// console.log(c);          // { d: 'world' }
+
+// TypeError: Cannot convert undefined or null to object
+// let a = null;
+// let b = {a: 2};
+// let c = Object.assign(a,b);
+// console.log(c);
+
+// let a = null;
+// let b = {a: 2};
+// let c = Object.assign(b,a);
+// console.log(c);                // { a: 2 }
