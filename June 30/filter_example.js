1c185492e75d0b5098324d3c42fae6302342d2f0
Pujan <pujan.jani@saeculumsolutions.com>
2023-07-03 09:36:42 +0530

    June 30

---
+++ June 30/filter_example.js
@@ -0,0 +1,203 @@
+// let arr = [1,2,3,4,5];
+// let newArr = arr.filter((value) => value < 4);
+// console.log(arr);
+// console.log(newArr);
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [1,2,3,4,5];
+// let newArr = arr.filter((value) => value = 5);
+// console.log(arr);
+// console.log(newArr);
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [1,2,3,4,5,8,2,5,7,3,5];
+// let newArr = arr.filter((value) => value == 5);
+// console.log(arr);
+// console.log(newArr);
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [1,2,3,4,5,8];
+// let newArr = arr.filter((value) => true);
+// console.log(arr);
+// console.log(newArr);
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [1,2,3,4,5,8];
+// let newArr = arr.filter((value) => false);
+// console.log(arr);
+// console.log(newArr);
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [1,2,3,4,5,8];
+// let newArr = arr.filter((value) => []);
+// console.log(arr);
+// console.log(newArr);
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [1,2,3,4,5,8];
+// let newArr = arr.filter((value) =>{ return {} });  // [1,2,3,4,5,8]
+// // let newArr = arr.filter((value) => {} );       // []
+// console.log(arr);
+// console.log(newArr);
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [1,2,3,4,5,8];
+// let newArr = arr.filter((value) => value%2 == 0 );
+// console.log(arr);
+// console.log(newArr);
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [1,2,3,4,1,8,10,3];
+// let newArr = arr.filter((value,index) => value < index);
+// console.log(arr);
+// console.log(newArr);
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [{a:1, b: 'p'}, {a:'j', b: 'p'}, {a:3, b: 'a'}, {b: 'p'}];
+// let newArr = arr.filter((item) => typeof item.a == 'number');
+// console.log(arr);
+// console.log(newArr);
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [1, {a:'j', b: 'p'}, 'a', [{b: 'p'}] ];
+// let newArr = arr.filter((item) => typeof item == 'object');
+// console.log(arr);
+// console.log(newArr);
+//------------------------------------------------------------------------------------------------------
+
+// let detail = [
+//     { fname: 'vijay', lname: 'bhatt', age: 25, city: 'rajkot'},
+//     { fname: 'rakesh', lname: 'joshi', age: 20, city: 'ahmedabad'},
+//     { fname: 'rahul', lname: 'jaikar', age: 30, city: 'baroda'},
+//     { fname: 'shankar', lname: 'bhatt', age: 25, city: 'mumbai'},
+//     { fname: 'vijay', lname: 'rajput', age: 25, city: 'rajkot'}
+// ]
+// let similar = detail.filter((person) => person.city == 'rajkot');
+// console.log(detail);
+// console.log(similar);
+
+//------------------------------------------------------------------------------------------------------
+
+// let detail = [
+//     { fname: 'vijay', lname: 'bhatt', age: 25, city: 'rajkot'},
+//     { fname: 'rakesh', lname: 'joshi', age: 20, city: 'ahmedabad'},
+//     { fname: 'rahul', lname: 'jaikar', age: 30, city: 'baroda'},
+//     { fname: 'shankar', lname: 'bhatt', age: 25, city: 'mumbai'},
+//     { fname: 'vijay', lname: 'rajput', age: 25, city: 'rajkot'}
+// ]
+// let similar = detail.filter((person) => person.age == 25 && person.city == 'rajkot');
+// console.log(detail);
+// console.log(similar);
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = ['apple','orange','mango','gauva','banana'];
+// let arr2 = arr.filter((value) => value.length > 5);
+// console.log(arr);
+// console.log(arr2);
+
+//------------------------------------------------------------------------------------------------------
+
+// const persons = [
+//     { name: 'devang', age: 16 },
+//     { name: 'raj', age: 22 },
+//     { name: 'Sameer', age: 14 },
+//     { name: 'Shanket', age: 19 },
+//   ];
+//   const mature = persons.filter((person) => person.age >= 18);
+//   console.log(mature); 
+
+//------------------------------------------------------------------------------------------------------
+
+// const numbers = [1, 2, 3, 4, 3, 2, 5, 6, 1];
+// const uniqueNumbers = numbers.filter((value, index, array) => array.indexOf(value) === index);
+// console.log(uniqueNumbers);
+
+//------------------------------------------------------------------------------------------------------
+
+// const words = ['apple', 'banana', 'orange', 'cherry'];
+// const arr = words.filter(word => word.includes('a'));
+// console.log(arr); 
+
+//------------------------------------------------------------------------------------------------------
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const arr2 = arr.filter((value, index) => index % 2 == 0);
+// console.log(arr2);
+
+//------------------------------------------------------------------------------------------------------
+
+// const books = [
+//     { title: 'Book 1', authors: ['Author 1', 'Author 2'] },
+//     { title: 'Book 2', authors: ['Author 2', 'Author 3', 'Author 4'] },
+//     { title: 'Book 3', authors: ['Author 1'] },
+//   ];
+//   const arr = books.filter(book => book.authors.length > 2);
+//   console.log(arr); 
+  
+//------------------------------------------------------------------------------------------------------
+
+// const arr = ['hello', '   ', 'world', '  ', ''];
+// const arr2 = strings.filter(word => word.trim() !== '');
+// console.log(arr2);
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [
+//     {a:10, b:20, c:[{a:11, b:21, c:31}]},
+//     {a:12, b:18, c:[{a:8, b:25, c:26}]},
+//     {a:16, b:29, c:[{a:5, b:10, c:0}]},
+//     {a:15, b:20, c:[{a:11, b:21, c:5}]}
+// ]
+// let arr2 = arr.filter((value) => value.c.filter((c)=> c.c < 20).length);
+// console.log(arr2);
+
+//------------------------------------------------------------------------------------------------------
+
+//  let arr = [1,2,'3','5',6,'7'];
+//  let arr2 = arr.filter((value) => typeof value === 'number');
+//  console.log(arr2);
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [
+//     {a:10, b:20, c:[{a:11, b:21, c:31}]},
+//     {a:12, b:18, c:[{a:8, b:25},{a:8, b:25}]},
+//     {a:16, b:29, c:[{a:5, b:10, c:0}]},
+//     {a:15, b:20, c:[{a:11, b:21, c:5},{a:11},{b:2}]}
+// ]
+// let arr2 = arr.filter((value) => value.c.length > 2);
+// console.log(arr2);
+
+//------------------------------------------------------------------------------------------------------
+
+// function isArrayContainNumber(input){
+//     let arr = input.filter((value) => typeof value === "number" && !isNaN(value))
+//     return arr;
+// }
+  
+//   console.log(isArrayContainNumber([1, 2, 3]));             
+//   console.log(isArrayContainNumber(["1", "a", "h"]));       
+//   console.log(isArrayContainNumber(["a", 3, "gh"]));        
+//   console.log(isArrayContainNumber(["a", '3df', "gh"])); 
+
+//------------------------------------------------------------------------------------------------------
+
+// let arr = [
+//     {a:10, b:20, c:[{a:11, b:21, d:['p','q']}]},
+//     {a:12, b:18, c:[{a:8, b:25, d:[1,2,'p']}]},
+//     {a:16, b:29, c:[{a:5, b:10, d:['q','r','s']}]},
+//     {a:15, b:20, c:[{a:11, b:21, d:5}]}
+// ]
+// let arr2 = arr.filter((value) => value.c.filter((cvalue)=> cvalue.d.filter((cvaluevalue) => cvaluevalue == 'p').length));
+// console.log(arr2);
---
+++ June 30/map_examples.js
@@ -0,0 +1,283 @@
+// const array = [{},{},{},{}];
+// const array2 = array.map((value) => {
+//       value.fname = 'pujan'; return value;
+// });
+// // const array2 = array.map((value) => value.fname = 'pujan');
+// console.log(array);
+// console.log(array2);
+
+//--------------------------------------------------------------------------------------------------
+
+// const arr = [1,2,3,4,5];
+// const arr2 = arr.map((value,index) => value*index);
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// const arr = [{a:15, b: 20}];
+// const arr2 = [{...arr}];
+// const newArr = arr2.map((value) => value.b = 30);
+// console.log(arr);
+// console.log(arr2);
+// console.log(newArr);
+
+//--------------------------------------------------------------------------------------------------
+
+// const arr = [15,20];
+// const arr2 = [{...arr}];
+// const newArr = arr2.map((value) => value.b = 30);
+// console.log(arr);
+// console.log(arr2);
+// console.log(newArr);
+
+//--------------------------------------------------------------------------------------------------
+
+// const names  = [
+// {first_name: "pujan", last_name: "jani"},
+// {first_name: "pratham", last_name: "pandya"},
+// {first_name: "Pavan", last_name: "chauhan"},
+// {first_name: "rohan", last_name: "sharaf"},
+// ]
+// let b = names.map((value)=>{value.full_name = value.first_name + ' ' +value.last_name;
+//                          return value;})
+// // console.log(names);
+// console.log(b);
+
+//--------------------------------------------------------------------------------------------------
+
+// function isArrayContainNumber(input){
+//     let arr = input.map((value) => {
+//         if(typeof value === "number" && !isNaN(value)){
+//             return true;
+//           }
+//         // return false;
+//     })
+//     return arr.includes(true);
+// }
+  
+//   console.log(isArrayContainNumber([1, 2, 3]));             
+//   console.log(isArrayContainNumber(["1", "a", "h"]));       
+//   console.log(isArrayContainNumber(["a", 3, "gh"]));        
+//   console.log(isArrayContainNumber(["a", '3df', "gh"]));    
+
+//--------------------------------------------------------------------------------------------------
+
+// function isArrayContainNumber(input){
+//     let arr = input.map((value) =>typeof value === "number" && !isNaN(value))
+//     return arr.includes(true);
+// }
+  
+//   console.log(isArrayContainNumber([1, 2, 3]));             
+//   console.log(isArrayContainNumber(["1", "a", "h"]));       
+//   console.log(isArrayContainNumber(["a", 3, "gh"]));        
+//   console.log(isArrayContainNumber(["a", '3df', "gh"]));   
+
+//--------------------------------------------------------------------------------------------------
+
+
+// const arr = [{
+//     0: 'a',
+//     1: 'b',
+//     2: 'c',
+//     3:  5
+// }]
+// const arr2 = arr.map((value) => {
+//     for(let key in value){
+//         console.log(value[key]);
+//     }
+// })
+
+//--------------------------------------------------------------------------------------------------
+
+// const arr = [10,15,35,80,42];
+// const arr2 = arr.map((value) => value > 40);
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// const str = ['hello world'];
+// const arr = str.map((value) => value.split(''));
+// console.log(str);
+// console.log(arr);
+
+//--------------------------------------------------------------------------------------------------
+
+// let obj1 = { a: 23, b: 45 };
+// let arr1 = [{...obj1}];
+// let arr2 = arr1.map((value) => value.b = 100);
+// console.log(arr1)
+
+//--------------------------------------------------------------------------------------------------
+
+// let arr = ['pujan','vidhan', 'devang', 'veer'];
+// let arr2 = arr.map((value) => value.charAt(0).toUpperCase() +  value.slice(1));
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// let arr = [[1,2], [3,4], [5,6,9], [7,8,10]];
+// let arr2 = arr.map((value,index) => value[index]);
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// let arr = [[],[],[],[]];
+// let arr2 = arr.map((value) =>{value[0] = 'fname'; return value;});
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// let arr = [[1,2], [3,4], [5,6,9], [7,8,10]];
+// let arr2 = arr.map((value) =>{
+//        let a =  value.map((value) => value*2);
+//        return a;
+// });
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// Doubt
+// let arr = [{a:10, b:[33,44]}];
+// let arr2 = arr.map((value) =>{
+//       for(let key in value){
+//          if(Array.isArray(value[key])){
+//               let a = value[key].map((value) => value*2 );
+//               return a;
+//          }
+//       }    
+// });
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// const fahrenheit= [32, 68, 86, 104];
+// const celsius= fahrenheit.map((value) => (value - 32) * 5/9);
+// console.log(celsius);
+
+//--------------------------------------------------------------------------------------------------
+
+// const arr = [
+//     {fname: 'pujan', age: 18},
+//     {fname: 'pujan', age: 18},
+//     {fname: 'pujan', age: 18},
+//     {fname: 'pujan', age: 18}
+// ];
+// const arr2 = arr.map((value) => delete value.age);
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// const arr = [1,2,3,4,5];
+// const arr2 = arr.map((value) => value = 20);
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// const arr = [1,2,[4,5],{a:6,b:7},[8,9,10]];
+// const arr2 = arr.map((value) => {
+//     if(Array.isArray(value)){
+//         return value;
+//     }
+//     else return false;
+// });
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// const arr = [10,20,30,40];
+// const arr2 = arr.map((value,index,array) => value + array);
+// console.log(arr);
+// console.log(arr2);  // [ '1010,20,30,40', '2010,20,30,40', '3010,20,30,40', '4010,20,30,40' ]
+
+
+// let a = 10 + [1,2,3,4];
+// console.log(a);
+// console.log(typeof a);
+
+//--------------------------------------------------------------------------------------------------
+
+// let arr = [1,2,3,4,5];
+// let arr2 = arr.map((value) => value*value);
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// let arr = [1,2,3,4,5];
+// let arr2 = arr.map((value,index,array) => array[index] - value);
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// let detail = [
+//     { fname: 'vijay', lname: 'bhatt', age: 25, city: 'rajkot'},
+//     { fname: 'rakesh', lname: 'joshi', age: 20, city: 'ahmedabad'},
+//     { fname: 'rahul', lname: 'jaikar', age: 30, city: 'baroda'},
+//     { fname: 'shankar', lname: 'bhatt', age: 25, city: 'mumbai'},
+//     { fname: 'vijay', lname: 'rajput', age: 25, city: 'rajkot'}
+// ]
+// let similar = detail.map((person) => person.fname + ' ' + person.lname);
+// console.log(detail);
+// console.log(similar);
+
+//--------------------------------------------------------------------------------------------------
+
+// let cities = ['Gondal','Rajkot','Ahmedabad','Mumbai'];
+// let length = cities.map((city) => city.length);
+// console.log(cities);
+// console.log(length);
+
+//--------------------------------------------------------------------------------------------------
+
+// let cities = ['Gondal','Rajkot','Ahmedabad','Mumbai'];
+// let newArr = cities.map((city) => {return {City: city}});
+// console.log(cities);
+// console.log(newArr);
+
+//--------------------------------------------------------------------------------------------------
+
+// let arr = [{a:1},{a:5,b:4},{c:6}];
+// let arr2 = arr.map((value) => { 
+//     value.b = 10;
+//     return value;
+// });
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// let arr = [{a:1},{a:5,b:4},{c:6}];
+// let arr2 = arr.map((value) => { 
+//     value.b = 10;
+//     return value;
+// });
+// console.log(arr);
+// console.log(arr2);
+
+//--------------------------------------------------------------------------------------------------
+
+// let arr = 'hello world!';
+// let arr2 = arr.split(' ');
+// let arr3 = arr2.map((word) => word.toUpperCase());
+// let arr4 = arr3.join(' ');
+// console.log(arr3);
+// console.log(arr4);
+
+//--------------------------------------------------------------------------------------------------
+
+// let arr = 'hello world';
+// let arr2 = arr.split(' ');
+// let arr3 = arr2.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+// let arr4 = arr3.join(' ');
+// console.log(arr4);
