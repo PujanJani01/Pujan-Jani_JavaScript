c4f8fdb8024eb9ebb2634dcd17e211ff01259819
Pujan <pujan.jani@saeculumsolutions.com>
2023-07-06 09:55:36 +0530

    reduce.js updated

--- July 4/reduce.js
+++ July 4/reduce.js
@@ -200,72 +200,89 @@
 
 
 // let arr = [];
-// let arr2 = arr.reduce((previous,current)=> previous-current);
+// let ans = arr.reduce((previous,current)=> previous-current);
 // console.log(arr2);           // TypeError: Reduce of empty array with no initial value
 
 // let arr = [];
-// let arr2 = arr.reduce((previous,current)=> previous-current, 10);
+// let ans = arr.reduce((previous,current)=> previous-current, 10);
 // console.log(arr2);     // 10
 
 // let arr = [];
-// let arr2 = arr.reduce((previous,current)=> previous-current,[10]);
+// let ans = arr.reduce((previous,current)=> previous-current,[10]);
 // console.log(arr2);   // [10]
 
 // let arr = [];
-// let arr2 = arr.reduce((previous,current)=> previous-current, {a:10});
+// let ans = arr.reduce((previous,current)=> previous-current, {a:10});
 // console.log(arr2);   // {a: 10}
 
 // let arr = [{a:10}];
-// let arr2 = arr.reduce((previous,current)=> previous-current,{a:10});
+// let ans = arr.reduce((previous,current)=> previous-current,{a:10});
 // console.log(arr2);  // NaN
 
 // let arr = [[10]];
-// let arr2 = arr.reduce((previous,current)=> previous-current,20);
+// let ans = arr.reduce((previous,current)=> previous-current,20);
 // console.log(arr2);  // 10
 
 // let arr = [[10],[25]];
-// let arr2 = arr.reduce((previous,current)=> previous-current,20);
+// let ans = arr.reduce((previous,current)=> previous-current,20);
 // console.log(arr2); // -15
 
 // let arr = [[10,3],[25]];
-// let arr2 = arr.reduce((previous,current)=> previous-current,20);
+// let ans = arr.reduce((previous,current)=> previous-current,20);
 // console.log(arr2);  // NaN
 
 // let arr = [[10],[25]];
-// let arr2 = arr.reduce((previous,current)=> previous+current,20);
+// let ans = arr.reduce((previous,current)=> previous+current,20);
 // console.log(arr2); // string - 201025
 
 // let arr = [1,2,3,4,5];
-// let arr2 = arr.reduce((previous,current)=> previous+current,[]);
+// let ans = arr.reduce((previous,current)=> previous+current,[]);
 // console.log(arr2);  // string - 12345 
 
 
 // let arr = [10,15,20,25,30];
-// let arr2 = arr.reduce((previous,current)=> previous+current,undefined);
+// let ans = arr.reduce((previous,current)=> previous+current,undefined);
 // console.log(arr2);  // NaN
 
 // let arr = [10,15,20,25,30];
-// let arr2 = arr.reduce((previous,current)=> previous+current,null);
+// let ans = arr.reduce((previous,current)=> previous+current,null);
 // console.log(arr2);  // 100
 
 // let arr = [10,15,20,25,30];
-// let arr2 = arr.reduce((previous,current)=> previous+current,[]);
+// let ans = arr.reduce((previous,current)=> previous+current,[]);
 // console.log(arr2);  // string - 1015202530
 
 // let arr = [10,15,20,25,30];
-// let arr2 = arr.reduce((previous,current)=> previous+current,{});
+// let ans = arr.reduce((previous,current)=> previous+current,{});
 // console.log(arr2);  // [object Object]1015202530
 
 
 // let arr = [1,20,13,4,5];
-// let arr2 = arr.sort().reduce((a,c) => [a+c]).map(value => value);
+// let ans = arr.sort().reduce((a,c) => [a+c]).map(value => value);
 // console.log(arr);  // [1,13,20,4,5]
 // console.log(arr2);  // [ '142045' ]
 
 // let arr = [{a:4},{b:5,c:6},{a:10},{c:3,d:2}];
-// let arr2 = arr.reduce((a,c) => Object.assign(a,c))
+// let ans = arr.reduce((a,c) => Object.assign(a,c))
 // console.log(arr2);
 
 // let arr = [{a:4},{b:5,c:6},{a:10},{c:3,d:2}];
-// let arr2 = arr.reduce((a,c) =>{return {...a,...c}})
+// let ans = arr.reduce((a,c) =>{return {...a,...c}})
 // console.log(arr2);
+
+// let arr = ['Js','practice','exercices'];
+// let ans = arr.reduce((a,c) => a + ' ' + c);
+// console.log(arr2);
+
+// let arr = [3,8,1,6,0];
+// let ans = arr.reduce((a,c) => a < c); 
+// console.log(ans);
+
+// let arr = [3,8,1,6,0];
+// let ans = arr.reduce((a,c) => a > c ? a : c); 
+// console.log(ans);   // 8
+
+// let arr = [3,8,1,6,0];
+// let ans = arr.reduce((a,c) => a < c ? a : c); 
+// console.log(ans);   // 0
+
