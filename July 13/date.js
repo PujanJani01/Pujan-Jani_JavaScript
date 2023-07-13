c8a03cce327af6d6f59e5d3dc26539e1b292771d
Pujan <pujan.jani@saeculumsolutions.com>
2023-07-13 11:03:35 +0530

    july 13

---
+++ July 13/date.js
@@ -0,0 +1,32 @@
+// function strToDate(datestr){
+//     let dt = new Date(datestr)
+//     return dt.toString();
+//  }
+//  console.log(strToDate("2005/1/30"));      // valid
+//  console.log(strToDate("2005/30/1"));      // invalid
+//  console.log(strToDate("30/1/2005"));      // invalid
+//  console.log(strToDate("1/30/2005"));      // valid
+
+//  console.log(strToDate("2005-1-30"));      // valid
+//  console.log(strToDate("2005-30-1"));      // invalid
+//  console.log(strToDate("30-1-2005"));      // invalid
+//  console.log(strToDate("1-30-2005"));      // valid
+
+//  console.log(strToDate("30 Jan 2005"));    // valid
+//  console.log(strToDate("Jan 30 2005"));    // valid
+//  console.log(strToDate("2005 30 Jan"));    // valid
+//  console.log(strToDate("2005 Jan 30"));    // valid
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+// let dt = new Date("2005/30/1");
+// // console.log(dt.toJSON());                 // null
+// // console.log(dt.toISOString());            // RangeError: Invalid time value
+// // console.log(dt.toUTCString());            // Invalid Date
+// // console.log(dt.toString());               // Invalid Date
+// // console.log(dt.toLocaleString());         // Invalid Date
+// // console.log(dt.toLocaleDateString());     // Invalid Date
+// // console.log(dt.toLocaleTimeString());     // Invalid Date
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
