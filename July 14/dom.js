6496e9a0b2ecccc690a7db3d7922279edc7e57c3
Pujan <pujan.jani@saeculumsolutions.com>
2023-07-14 19:26:11 +0530

    commit before leaving

--- July 14/dom.js
+++ July 14/dom.js
@@ -61,6 +61,7 @@ document.getElementById('div1').appendChild(comment);
 // console.log(a.classList.contains('pujan'));
 // console.log(a.classList.contains('jani'));
 
+// console.log(a.classList);
 //----------------------------------------------------------------------------------------------
 
 // let a = document.getElementById('div1').getElementsByTagName('p');
@@ -71,3 +72,67 @@ document.getElementById('div1').appendChild(comment);
 // a[0].innerHTML = 'hello guys!';
 
 // console.log(a[0].innerHTML);
+
+//-----------------------------------------------------------------------------------------------
+
+// let a = document.getElementsByClassName('link');
+// // let a = document.getElementById('div1').getElementsByClassName('link');
+// console.log(a);
+
+// console.log(a[0].attributes);     //  return NamedNodeMap
+// console.log(a[0].attributes[0]);  // return attribute at 0th index in NamedNodeMap with content
+// console.log(a[0].attributes[1]);  // return attribute at 1th index in NamedNodeMap with content
+
+// a[0].setAttribute('id','google');
+// console.log(a[0].attributes);
+// console.log(a[0].attributes[2]);
+
+//-----------------------------------------------------------------------------------------------
+
+//   console.log(document.getElementById('div1'));
+//   console.log(document.getElementById('div1').innerHTML);
+//   console.log(document.getElementById('div1').outerHTML);
+
+//-----------------------------------------------------------------------------------------------
+
+// let child1 = document.getElementById('div1');
+
+// console.log(child1.firstElementChild);     // <p id="para" class="para">How are you?</p>
+// console.log(child1.lastElementChild);      // <a href="https://google.com" class="link"  target="_blank">click to go to google</a>
+  
+
+// let child2 = document.getElementById('para');
+
+// console.log(child2.firstChild);    // How are you?
+// console.log(child2.lastChild);     // How are you?
+
+//-----------------------------------------------------------------------------------------------
+
+// let sibling = document.getElementById('para');
+// console.log(sibling.nextElementSibling);       // <p class="para">How</p>
+// console.log(sibling.previousElementSibling);   // null
+
+//-----------------------------------------------------------------------------------------------
+
+// let tag = document.getElementById('para');
+// console.log(tag.tagName);   // P
+
+// let tag2 = document.getElementsByClassName('para');
+// for(let i = 0 ; i < tag2.length; i++){
+//     console.log(tag2[i].tagName);
+// }
+
+// let tag3 = document.getElementsByTagName('p');
+// for(let i = 0 ; i < tag3.length; i++){
+//     console.log(tag3[i].tagName);
+// }
+
+//-----------------------------------------------------------------------------------------------
+
+// console.log(document.getElementById('para').getAttributeNames());
+
+// let t = document.getElementById('para');
+// console.log(t.getAttributeNode('id'));   // id = 'para'
+// console.log(t.getAttributeNode('class'));   // class = 'para'
+
+
---
+++ July 14/new.js
@@ -0,0 +1,55 @@
+
+// const arr = [
+//     {
+//         id: 1,
+//         slot: [
+//              { dosage: ''}, {dosage: 'hi'}
+//         ]
+//     },
+//     {
+//         id: 2,
+//         slot: [
+//              { dosage: 'hi'}, {dosage: 'hi'}
+//         ]
+//     }
+// ]
+
+// function abc(arr){
+//   if(arr.every(el => el.slot.every(value => value.dosage != ''))){
+//       return 'Dosage done';
+//   }
+//   return 'No Dosage';
+// }
+// console.log(abc(arr));
+
+
+//---------------------------------------------------------------------------------------------
+
+// function abc(){
+//     const ab = () => 40;
+//     abcd(() => ab());
+// }
+// function abcd(cb){
+//     let a = cb();
+//     console.log(a);        // 40
+// }
+// console.log(abc());        // undefined
+ 
+//---------------------------------------------------------------------------------------------
+
+// function getName(obj,key){
+//     return obj.key;
+// }
+// const obj1 = { name: 'demo', age: 23};
+// let name = getName(obj1,'name');
+// console.log(name);             // undefined
+
+//---------------------------------------------------------------------------------------------
+
+// let obj = { df: 56};
+// let {a: df = 45 ,b = 34} = obj;
+// console.log(b);
+
+//---------------------------------------------------------------------------------------------
+
+
