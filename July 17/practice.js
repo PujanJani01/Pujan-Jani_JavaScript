4606dfa61f774ef9b1b893eb612f1e61ee2bc854
Pujan <pujan.jani@saeculumsolutions.com>
2023-07-17 19:50:42 +0530

    last commit

---
+++ July 17/practice.js
@@ -0,0 +1,5 @@
+// function abc({a:b,b}){
+//     console.log(b);
+// }
+// abc({a:10, b:20});
+
--- July 17/script.js
+++ July 17/script.js
@@ -1,32 +1,44 @@
-let nav = document.getElementsByTagName('li');
-for(let i = 0; i<nav.length; i++){
-    nav[i].addEventListener("click", () =>{
-        nav[i].style.borderBottom = "solid";
+let nav = document.querySelectorAll('li');
+
+// nav[0].style.borderBottom = "solid";
+// for(let i = 0; i<nav.length; i++){
+//     nav[i].addEventListener("click", () =>{
+//         nav[i].style.borderBottom = "solid";
+//     })
+//     for(let j = i+1 ; j < nav.length ; j++){
+//         nav[j].addEventListener("click", () =>{
+//             nav[i].style.borderBottom = "";
+//         })
+//         nav[i].addEventListener("click", () =>{
+//             nav[j].style.borderBottom = "";
+//         })
+//     }
+// }
+
+nav[0].className="active";  
+nav.forEach(el =>{
+    el.addEventListener('click', () =>{
+        document.querySelector(".active")?.classList.remove("active");
+        el.classList.add("active");
     })
-    for(let j = i+1 ; j < nav.length ; j++){
-        nav[j].addEventListener("click", () =>{
-            nav[i].style.borderBottom = "";
-        })
-        nav[i].addEventListener("click", () =>{
-            nav[j].style.borderBottom = "";
-        })
-    }
+})
+
+var ul = document.getElementById('nav-list');
+var bar = document.getElementsByClassName('bar')[0];
+var ul2 = ul.cloneNode(true);
+document.getElementsByClassName("menubar")[0].appendChild(ul2);
+
+bar.addEventListener('click', abc)
+function abc(){
+    document.getElementsByClassName("menubar")[0].style.right = "0%";    
+    
+    //  document.getElementsByClassName("menubar")[0].classList.toggle('active');
+    ul2.style.display = "flex";
 }
 
-
-let ul = document.getElementById('nav-list');
-let bar = document.getElementsByClassName('bar')[0];
-
-bar.addEventListener('click', () => {
-    document.getElementsByClassName("menubar")[0].style.display = "block";
-     document.getElementsByClassName("menubar")[0].style.left = "0%";
-    ul.style.display = "flex"; 
-    document.getElementsByClassName("menubar")[0].appendChild(ul);
-    
-})
-
 let closebtn = document.getElementsByClassName('closebtn')[0];
-
 closebtn.addEventListener("click", () =>{
-    document.getElementsByClassName("menubar")[0].style.display = "none";
+    document.getElementsByClassName("menubar")[0].style.right = "100%";
+    document.getElementsByClassName("menubar")[0].style.transition = "all 2s";
 })
+
