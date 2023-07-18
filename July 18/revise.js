c00d3a5dbdb35bfc2c81a418a65e41c1f8fa5ee7
Pujan <pujan.jani@saeculumsolutions.com>
2023-07-18 11:38:05 +0530

    commit before session

---
+++ July 18/revise.js
@@ -0,0 +1,11 @@
+let arr = [1,3,6,8,2];
+const obj = [
+    {id:1, fname:'Anil'},
+    {id:3, fname:'Ramesh'},
+    {id:5, fname:'Sujal'},
+    {id:8, fname:'Kunal'},
+]
+
+function notContainsId(){
+    
+}
---
+++ July 18/script.js
@@ -0,0 +1,9 @@
+// let txt = document.getElementsByClassName('para')[0];
+// console.log(txt.innerHTML);     // Hello <span>World!</span>
+// console.log(txt.innerText);     // Hello World!
+// console.log(txt.textContent);   // Hello World!
+
+
+// console.log(document.getElementsByTagName('script')[0].innerHTML);
+// console.log(document.getElementsByTagName('script')[0].innerText);
+// console.log(document.getElementsByTagName('script')[0].textContent);
