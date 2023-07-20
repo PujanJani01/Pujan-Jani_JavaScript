9bb8d0c134a8711a58fe58d05f0df3eeac38ead8
Pujan <pujan.jani@saeculumsolutions.com>
2023-07-20 17:12:52 +0530

    commit before coffee

---
+++ July 20/new.js
@@ -0,0 +1,17 @@
+/* const arr = [
+    { name: 'John', location: 'Los Angeles' },
+    { name: 'Kate', location: 'New York' },
+    { name: 'Kate', location: 'New York' },
+    { name: 'Kate', location: 'LA' },
+    { name: 'Mike', location: 'New York' },
+  ];
+  
+  const unique = [];
+  arr.forEach((obj) => {
+    const found = unique.findIndex((item) => item.name === obj.name && item.location === obj.location);
+    if (found === -1) {
+      unique.push(obj);
+    }
+  });
+  console.log(unique);  */
+  
--- July 20/script.js
+++ July 20/script.js
@@ -19,7 +19,7 @@ console.log(script.textContent); */
 /* para.addEventListener("mouseleave", () =>{
     para.style.color = 'blue';
 })  */
- 
+
 /* para.addEventListener("mousemove", () =>{
     para.style.color = 'blue';
 }) */
@@ -91,12 +91,12 @@ list.addEventListener('click', () =>{
     })    
 }) */
 
-/* let input = document.querySelector('input');
+let input = document.querySelector('#user');
 input.addEventListener('keyup', () =>{
     input.style.backgroundColor = 'red'
-}) */
+})
 
-/* let input = document.querySelector('input');
+/* let input = document.querySelector('#user');
 input.addEventListener('keydown', () =>{
     input.style.backgroundColor = 'red'
 }) */
@@ -105,6 +105,85 @@ input.addEventListener('keydown', () =>{
 console.log(list.firstChild);
 console.log(list.firstElementChild); */
 
-let listItems =  document.querySelector('li');
+/* let listItems =  document.querySelector('li');
 console.log(listItems.nextSibling);
-console.log(listItems.nextElementSibling);
+console.log(listItems.nextElementSibling); */
+
+/* let input = document.querySelector('#pass');
+input.addEventListener('focus', () =>{
+    input.style.backgroundColor = 'pink';
+})
+input.addEventListener('blur', () =>{
+    input.style.backgroundColor = '';
+}) */
+
+let input2 = document.querySelector('#pass');
+input2.addEventListener('focusin', () =>{
+    input2.style.backgroundColor = 'pink';
+})
+input2.addEventListener('focusout', () =>{
+    input2.style.backgroundColor = '';
+}) 
+
+/*  let body = document.querySelector('body');
+window.addEventListener('resize', () =>{
+    document.querySelector('ul').style.listStyleType = 'none' 
+    document.querySelector('ul').style.display = 'flex' 
+    document.querySelector('ul').style.justifyContent = 'space-around' 
+}) */
+
+let dropboxes = document.querySelectorAll('.dropDiv');
+let dragItems = document.querySelectorAll('.dragItem');
+
+dragItems.forEach(item =>{
+    item.addEventListener('dragstart', () =>{
+        item.classList.add('dragging');
+    })
+    item.addEventListener('dragend', () =>{
+        item.classList.remove('dragging');
+    })
+})
+
+dropboxes.forEach(box =>{
+    box.addEventListener('dragover', (e) =>{
+        e.preventDefault();
+        const draggable = document.querySelector('.dragging');
+        box.appendChild(draggable);
+    })
+})
+
+let checkbox = document.querySelector('#check');
+checkbox.addEventListener('click', (e) =>{
+    e.preventDefault();
+})
+
+let checkbtn = document.querySelector('#checkbtn');
+checkbtn.addEventListener('click', () => {
+    if(checkbox.hasAttribute('checked')){
+        checkbox.removeAttribute('checked');
+        checkbtn.innerText = "check";
+    }
+    else if(!checkbox.hasAttribute('checked')){
+        checkbox.setAttribute('checked','');
+        checkbtn.innerText = "uncheck";
+    }
+})
+
+let inputbox = document.querySelector('#input');
+let txtbtn = document.querySelector('#txtbtn');
+txtbtn.addEventListener('click', () => {
+    if(inputbox.hasAttribute('disabled')){
+        inputbox.removeAttribute('disabled');
+        txtbtn.innerText = "disable";
+    }
+    else if(!inputbox.hasAttribute('checked')){
+        inputbox.setAttribute('disabled','');
+        txtbtn.innerText = "write";
+    }
+})
+
+
+
+
+
+
