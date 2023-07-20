0f084dd09b3ce5528628b342c137166a815e194e
Pujan <pujan.jani@saeculumsolutions.com>
2023-07-20 13:12:01 +0530

    commit before lunch

---
+++ July 20/script.js
@@ -0,0 +1,110 @@
+let para = document.getElementById('para');
+console.log(para.innerHTML);
+console.log(para.innerText);
+console.log(para.textContent);
+
+/* let script = document.querySelector('script');
+console.log(script.innerHTML);
+console.log(script.innerText);
+console.log(script.textContent); */
+
+/* para.addEventListener("mouseenter", () =>{
+    para.style.color = 'blue';
+}) */
+
+/* para.addEventListener("mouseover", () =>{
+    para.style.color = 'blue';
+}) */
+
+/* para.addEventListener("mouseleave", () =>{
+    para.style.color = 'blue';
+})  */
+ 
+/* para.addEventListener("mousemove", () =>{
+    para.style.color = 'blue';
+}) */
+
+/* para.addEventListener("mousedown", () =>{
+    para.style.color = 'blue';
+})
+ */
+/* para.addEventListener("mouseup", () =>{
+    para.style.color = 'blue';
+}) */
+
+/* para.addEventListener("click", () =>{
+    para.style.color = 'blue';
+}) */
+
+/* para.addEventListener("mouseout", () =>{
+    para.style.color = 'blue';
+}) */
+
+/* let list = document.querySelector('ul');
+list.addEventListener('mouseenter', () =>{
+    document.querySelectorAll('li').forEach(el =>{
+        el.innerText += ' 1'
+    })
+         
+}) */
+
+
+/* let list = document.querySelector('ul');
+list.addEventListener('mouseover', () =>{
+    document.querySelectorAll('li').forEach(el =>{
+        el.innerText += ' 1'
+    })    
+}) */
+
+/* let list = document.querySelector('ul');
+list.addEventListener('mousemove', () =>{
+    document.querySelectorAll('li').forEach(el =>{
+        el.innerText += ' 1'
+    })     
+}) */
+
+/* let list = document.querySelector('ul');
+list.addEventListener('mouseleave', () =>{
+    document.querySelectorAll('li').forEach(el =>{
+        el.innerText += ' 1'
+    })    
+}) */
+
+/* let list = document.querySelector('ul');
+list.addEventListener('mouseout', () =>{
+    document.querySelectorAll('li').forEach(el =>{
+        el.innerText += ' 1'
+    })    
+}) */
+
+/* let list = document.querySelector('ul');
+list.addEventListener('mouseup', () =>{
+    document.querySelectorAll('li').forEach(el =>{
+        el.innerText += ' 1'
+    })    
+}) */
+
+/* let list = document.querySelector('ul');
+list.addEventListener('click', () =>{
+    document.querySelectorAll('li').forEach(el =>{
+        el.innerText += ' 1'
+    })    
+}) */
+
+/* let input = document.querySelector('input');
+input.addEventListener('keyup', () =>{
+    input.style.backgroundColor = 'red'
+}) */
+
+/* let input = document.querySelector('input');
+input.addEventListener('keydown', () =>{
+    input.style.backgroundColor = 'red'
+}) */
+
+/* let list = document.querySelector('ul');
+console.log(list.firstChild);
+console.log(list.firstElementChild); */
+
+let listItems =  document.querySelector('li');
+console.log(listItems.nextSibling);
+console.log(listItems.nextElementSibling);
