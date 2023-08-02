41636cf79c937c72c9f31342fce81d2c3a1e8af2
Pujan <pujan.jani@saeculumsolutions.com>
2023-08-02 16:11:43 +0530

    commit before coffee

--- July 26/carousel/script.js
+++ July 26/carousel/script.js
@@ -1,37 +1,37 @@
 let left = document.querySelector('#left');
 let right = document.querySelector('#right');
 let pics = document.querySelector('.pics');
+let carosuel = document.querySelector('.visiblediv');
 let direction;
 
 right.addEventListener('click', () => {
+  direction = -1;
+  carosuel.style.justifyContent = 'flex-start';
   pics.style.transform = `translateX(-25%)`;
-  goRight();
-})
-left.addEventListener('click', () => {
-  pics.style.transform = `translateX(25%)`;
-  goLeft();
 })
 
-function goRight(){
-  pics.addEventListener('transitionend', () => {
+left.addEventListener('click', () => {
+  if(direction == -1){
+    direction = 1;
     pics.appendChild(pics.firstElementChild);
+  }
+  carosuel.style.justifyContent = 'flex-end';
+  pics.style.transform = `translateX(25%)`;
+})
+
+  pics.addEventListener('transitionend', () => {
+    if(direction == -1){
+      pics.appendChild(pics.firstElementChild);
+    }
+    else if(direction == 1){
+    pics.prepend(pics.lastElementChild);
+    }
     pics.style.transition = 'none';
     pics.style.transform = 'translateX(0)';
     setTimeout(() => {
       pics.style.transition = 'all 1s';
     })
   })
-}
 
-// function goLeft(){
-//   pics.addEventListener('transitionstart', () => {
-//     pics.prepend(pics.lastElementChild);
-//     pics.style.transform = 'translateX(0)';
-//     setTimeout(() => {
-//       pics.style.transition = 'all 1s';
-//     })
-//     pics.style.transition = 'none';
-//   })
-// }
 
 
